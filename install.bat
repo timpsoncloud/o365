3ab6a3ee9c451ead9e8f35d5937b51fe75256f51
timpsoncloud <infrastructure@timpson.com>
2025-05-13 21:04:56 +0100

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,6 @@
+@echo off
+powershell -Command "Get-AppxPackage -AllUsers 'Microsoft.OutlookForWindows' | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue" 
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\WorkplaceJoin" /v BlockAADWorkplaceJoin /t REG_DWORD /d 1 /f
+"C:\O365\setup.exe" /configure "C:\O365\config.xml"
+schtasks /delete /tn "Deploy M365 Apps" /f
+shutdown /f /r /t 0
