18ad9fe83feda844822441f606e197312e99642d
timpsoncloud <infrastructure@timpson.com>
2025-05-13 21:22:31 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,4 +3,5 @@ powershell -Command "Get-AppxPackage -AllUsers 'Microsoft.OutlookForWindows' | R
 REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\WorkplaceJoin" /v BlockAADWorkplaceJoin /t REG_DWORD /d 1 /f
 "C:\O365\setup.exe" /configure "C:\O365\config.xml"
 schtasks /delete /tn "Deploy M365 Apps" /f
+rmdir /s /q C:\o365
 shutdown /f /r /t 0
