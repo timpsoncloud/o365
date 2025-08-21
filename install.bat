1b4f21e6761b7186aa5fc169b4443de6da28cfd6
timpsoncloud <infrastructure@timpson.com>
2025-08-21 19:14:23 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 @echo off
-REM powershell -Command "Get-AppxPackage -AllUsers 'Microsoft.OutlookForWindows' | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue" 
-REM REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\WorkplaceJoin" /v BlockAADWorkplaceJoin /t REG_DWORD /d 1 /f
+powershell -Command "Get-AppxPackage -AllUsers 'Microsoft.OutlookForWindows' | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue" 
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\WorkplaceJoin" /v BlockAADWorkplaceJoin /t REG_DWORD /d 1 /f
 "C:\O365\setup.exe" /configure "C:\O365\config.xml"
 schtasks /delete /tn "Deploy M365 Apps" /f
-REM rmdir /s /q C:\o365 & shutdown /f /r /t 0
+rmdir /s /q C:\o365 & shutdown /f /r /t 0
+
